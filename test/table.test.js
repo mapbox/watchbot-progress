2cc0fba8166626b9e12eb2108e11bb06f8cbc8cb
Emily DuBois <dubois@mapbox.com>
2016-12-06 16:24:40 -0500

    Adds progress table condition (#4)
    * Adds condition option for progress table

--- test/table.test.js
+++ test/table.test.js
@@ -1,7 +1,7 @@
 var table = require('../lib/table');
 var tape = require('tape');
 
-tape('[table] success', function(assert) {
+tape('[table] success, no condition', function(assert) {
   var name = 'test-name';
   var throughput = {
     readCapacityUnits: 10,
@@ -10,6 +10,40 @@ tape('[table] success', function(assert) {
 
   assert.deepEqual(table(name, throughput), {
     Type: 'AWS::DynamoDB::Table',
+    Properties: {
+      TableName: 'test-name',
+      AttributeDefinitions: [
+        {
+          AttributeName: 'id',
+          AttributeType: 'S'
+        }
+      ],
+      KeySchema: [
+        {
+          AttributeName: 'id',
+          KeyType: 'HASH'
+        }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+      }
+    }
+  });
+  assert.end();
+});
+
+tape('[table] success, condition', function(assert) {
+  var name = 'test-name';
+  var throughput = {
+    readCapacityUnits: 10,
+    writeCapacityUnits: 10
+  };
+  var condition = 'UseReduce';
+
+  assert.deepEqual(table(name, throughput, condition), {
+    Type: 'AWS::DynamoDB::Table',
+    Condition: condition,
     Properties: {
       TableName: 'test-name',
       AttributeDefinitions: [
